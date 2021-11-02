7098c8b9d64af0163bfca082cc9447984fdb2c10
iikim <iikim2511@gmail.com>
2021-11-01 23:05:47 -0400

    ready

---
+++ variable.js
@@ -0,0 +1,15 @@
+
+//  🍀let : 선언하고, 밑에서 알고리즘 코딩
+
+let number;
+let n = 0;
+let position = {x:0, y:0};
+
+// 🍀const : 기본 element
+
+const todoContainer = document.querySelector('.todo_container');
+const inputBox = document.querySelector('.input_box');
+const submitBtn = document.querySelector('.submit');
+
+let windowsContainer = document.querySelector('.windows_container');
+let covidWindow = document.querySelector('.covid_window');
