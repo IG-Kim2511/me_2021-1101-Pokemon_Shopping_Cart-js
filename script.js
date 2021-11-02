4146abe6c9dfc56162d8b3f592c1d65944066275
iikim <iikim2511@gmail.com>
2021-11-02 12:16:29 -0400

    addToCartClicked

--- script.js
+++ script.js
@@ -4,4 +4,13 @@ document.addEventListener('DOMContentLoaded',init);
 
 function init() {
     
+    // 🍖1102-4
+   for (let i = 0; i < shopItemBtnAll.length; i++) {
+    shopItemBtnAll[i].addEventListener('click',addToCartClicked);
+   }
+}
+
+/* 🍀1102-4 addToCartClicked*/
+function addToCartClicked(){
+
 }
